b453af6caa95c5403c68e55bc5b66f86e0953a11
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-04-19 13:27:11 +0200

    some file for the hepmc interface, not used AFAIK

--- IOMC/Input/test/testReader_cfg.py
+++ IOMC/Input/test/testReader_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("GEN")
 
 process.source = cms.Source("MCFileSource",
-	fileNames = cms.untracked.vstring('file:GenEvent_ASCII.dat')
+	fileNames = cms.untracked.vstring('file:../../../crmc_eposlhc_347761894_p_p_6500.hepmc')
 )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
