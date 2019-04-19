b6f1da0de11c0e579afdd4ca022daeee0d12d619
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-04-19 13:26:48 +0200

    some file for the LHE interface, not used AFAIK

--- GeneratorInterface/LHEInterface/test/testReader_cfg.py
+++ GeneratorInterface/LHEInterface/test/testReader_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("LHE")
 
 process.source = cms.Source("LHESource",
-	fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/l/lgray/work/public/CMSSW_7_0_0_pre2/src/2.0.0beta4/VBF_HAA_reweight/Events/VBF_HAA_reweight4/unweighted_events.lhe')
+	fileNames = cms.untracked.vstring('file:../../../crmc_eposlhc_659339566_p_p_6500.lhe')
 )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
@@ -21,7 +21,7 @@ process.MessageLogger.cout = cms.untracked.PSet( threshold = cms.untracked.strin
 
 process.LHE = cms.OutputModule("PoolOutputModule",
 	dataset = cms.untracked.PSet(dataTier = cms.untracked.string('LHE')),
-	fileName = cms.untracked.string('lhe.root')
+	fileName = cms.untracked.string('../../../crmc_eposlhc_659339566_p_p_6500.root')
 )
 
 #process.lhedump = cms.EDAnalyzer("DummyLHEAnalyzer",
