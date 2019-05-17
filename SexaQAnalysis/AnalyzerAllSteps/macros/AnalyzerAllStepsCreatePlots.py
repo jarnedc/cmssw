6124151474430823f086514f9afc96e5ee1fe047
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-05-17 16:26:14 +0200

    starting the python macro for extracting plots

--- SexaQAnalysis/AnalyzerAllSteps/macros/AnalyzerAllStepsCreatePlots.py
+++ SexaQAnalysis/AnalyzerAllSteps/macros/AnalyzerAllStepsCreatePlots.py
@@ -2,21 +2,21 @@ from ROOT import TFile, TH1F, TH2F, TEfficiency
 
 
 #fIn = TFile('/user/jdeclerc/Analysis/SexaQuark/CMSSW_9_4_7/src/SexaQAnalysis/AnalyzerAllSteps/test/wihtMatchingOnHits/test_TrackMatchingOnHits.root', 'read')
-fIn = TFile('/user/jdeclerc/CMSSW_8_0_30/src/SexaQAnalysis/AnalyzerAllSteps/test/wihtMatchingOnHits/test_TrackMatchingOnHits_withSexaq.root', 'read')
+fIn = TFile('/user/jdeclerc/CMSSW_8_0_30/src/SexaQAnalysis/AnalyzerAllSteps/test/wihtMatchingOnHits/test_TrackMatchingOnHits_withSexaq_Step2.root', 'read')
 
 aNum = [
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/RECO/h_NonAntiSTrack_RECO_pt"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/RECO/h_NonAntiSTrack_RECO_pt_cut_eta_Lxy"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/RECO/h_NonAntiSTrack_RECO_lxy_cut_pt_eta"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/RECO/h_NonAntiSTrack_RECO_eta_cut_pt_lxy")
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/RECO/h_NonAntiSTrack_RECO_pt"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/RECO/h_NonAntiSTrack_RECO_pt_cut_eta_Lxy"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/RECO/h_NonAntiSTrack_RECO_lxy_cut_pt_eta"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/RECO/h_NonAntiSTrack_RECO_eta_cut_pt_lxy")
 ]
 
 
 aDeNom = [
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/All/h_NonAntiSTrack_All_pt"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/All/h_NonAntiSTrack_All_pt_cut_eta_Lxy"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/All/h_NonAntiSTrack_All_lxy_cut_pt_eta"),
-fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiS/All/h_NonAntiSTrack_All_eta_cut_pt_lxy")
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/All/h_NonAntiSTrack_All_pt"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/All/h_NonAntiSTrack_All_pt_cut_eta_Lxy"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/All/h_NonAntiSTrack_All_lxy_cut_pt_eta"),
+fIn.Get("AnalyzerAllSteps/TrackingEff/NonAntiSTracks/All/h_NonAntiSTrack_All_eta_cut_pt_lxy")
 ]
 
 fOut = TFile('AnalyzerAllStepsPlots.root','RECREATE')
