edeeb6e24553d2fcb8a9a2ff33332fcc2864506c
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-05-23 14:11:35 +0200

    forgotten in previous commit

--- SexaQAnalysis/AnalyzerAllSteps/python/AnalyzerAllSteps_cfi.py
+++ SexaQAnalysis/AnalyzerAllSteps/python/AnalyzerAllSteps_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from Validation.RecoTrack.TrackingParticleSelectionForEfficiency_cfi import * 
 from SimTracker.TrackAssociation.LhcParametersDefinerForTP_cfi import * 
 AnalyzerAllSteps = cms.EDAnalyzer('AnalyzerAllSteps',
-    isData = cms.untracked.bool(True),
+    lookAtAntiS = cms.untracked.bool(False),
     beamspot = cms.InputTag("offlineBeamSpot"),
     offlinePV = cms.InputTag("offlinePrimaryVertices","","RECO"),
     genCollection_GEN =  cms.InputTag("genParticles","","GEN"),
@@ -11,7 +11,6 @@ AnalyzerAllSteps = cms.EDAnalyzer('AnalyzerAllSteps',
     sexaqCandidates = cms.InputTag("lambdaKshortVertexFilter", "sParticles","SEXAQ"),
     V0KsCollection = cms.InputTag("generalV0Candidates","Kshort","RECO"),
     V0LCollection = cms.InputTag("generalV0Candidates","Lambda","RECO"),
-    #trackAssociators = cms.InputTag("trackingParticleRecoTrackAsssociation"),
     trackAssociators = cms.InputTag("quickTrackAssociatorByHits"),
     TrackingParticles = cms.InputTag("mix","MergedTrackTruth")
 )
