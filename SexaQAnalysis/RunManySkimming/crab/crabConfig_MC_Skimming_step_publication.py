1edd1ff641ca03df3d2cabf11c0e563f84ffef41
Jarne Theo De Clercq <jarne.theo.de.clercq@cern.ch>
2019-08-18 10:08:26 +0200

    some changes to a cfg file

--- SexaQAnalysis/RunManySkimming/crab/crabConfig_MC_Skimming_step_publication.py
+++ SexaQAnalysis/RunManySkimming/crab/crabConfig_MC_Skimming_step_publication.py
@@ -1,6 +1,6 @@
 from WMCore.Configuration import Configuration
 
-day = "11082019"
+day = "18082019"
 version = "v6"
 
 config = Configuration()
@@ -8,6 +8,7 @@ config.section_('General')
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.General.requestName = 'SkimmingSexaq_DoubleMuonData_'+day+'_'+version 
+#config.General.requestName = 'SkimmingSexaq_ZJetsMC_'+day+'_'+version 
 
 config.section_('JobType') 
 config.JobType.pluginName = 'Analysis' 
@@ -15,7 +16,7 @@ config.JobType.psetName = 'treeproducer_data_cfg.py'
 
 config.section_('Data') 
 config.Data.unitsPerJob = 1
-config.Data.totalUnits = 72
+config.Data.totalUnits = 3978
 config.Data.publication = False 
 config.Data.splitting = 'FileBased' 
 config.Data.outLFNDirBase = '/store/user/jdeclerc/crmc_Sexaq/Skimmed/DataMCComparison' 
@@ -27,7 +28,8 @@ config.Data.outLFNDirBase = '/store/user/jdeclerc/crmc_Sexaq/Skimmed/DataMCCompa
 #config.Data.inputDataset = '/MinimumBias/PARun2016B-PromptReco-v1/RECO'
 
 #config.Data.inputDataset = '/ttZJets_13TeV_madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM'
-config.Data.inputDataset = '/DoubleMuon/Run2016C-07Aug17-v1/AOD'
+#config.Data.inputDataset = '/ttZJets_13TeV_madgraphMLM/RunIISummer16DR80-80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM'
+config.Data.inputDataset = '/DoubleMuon/Run2016H-07Aug17-v1/AOD'
 
 
 config.Data.inputDBS = 'global'
@@ -38,5 +40,5 @@ config.section_('User')
 #config.User.voGroup = 'becms'
 
 config.section_('Site') 
-config.Site.whitelist = ['T2_BE_IIHE'] 
+config.Site.whitelist = ['T2_BE_IIHE','T2_AT_Vienna','T2_BE_UCL','T2_CH_CERN_HLT','T2_DE_DESY','T2_DE_RWTH','T2_FR_IPHC','T1_DE_KIT','T1_UK_RAL','T2_HU_Budapest','T2_IT_Bari','T2_IT_Legnaro','T2_IT_Pisa'] 
 config.Site.storageSite = 'T2_BE_IIHE'
